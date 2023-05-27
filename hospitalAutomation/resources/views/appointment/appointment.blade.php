98d21a9b195c2522e0446706f3051978751ebdab
Yusufbzkrt <yusuf.bozkurt1593@gmail.com>
2023-05-27 19:54:51 +0300

    appointment frontendi, departman ve randevu saati içeriği düzeltildi

--- hospitalAutomation/resources/views/appointment/appointment.blade.php
+++ hospitalAutomation/resources/views/appointment/appointment.blade.php
@@ -35,34 +35,34 @@
         </div>
         <div class="user-box">
             <label>Randevu saatini seçiniz</label> <br> <br>
-            <select name="appoint_time" style="max-width:100px">
-                <option value="0">------</option>
-                <option value="1">9:00</option>
-                <option value="2">9:15</option>
-                <option value="3">9:30</option>
-                <option value="1">9:45</option>
-                <option value="2">10:00</option>
-                <option value="3">10:15</option>
-                <option value="1">10:30</option>
-                <option value="2">10:45</option>
-                <option value="2">11:00</option>
-                <option value="3">11:15</option>
-                <option value="1">11:30</option>
-                <option value="2">11:45</option>
-                <option value="1">13:30</option>
-                <option value="2">13:45</option>
-                <option value="3">14:00</option>
-                <option value="3">14:15</option>
-                <option value="1">14:30</option>
-                <option value="2">14:45</option>
-                <option value="2">15:00</option>
-                <option value="3">15:15</option>
-                <option value="1">15:30</option>
-                <option value="2">15:45</option>
-                <option value="2">16:00</option>
-                <option value="3">16:15</option>
-                <option value="1">16:30</option>
-                <option value="2">16:45</option>
+            <select name="appoint_time"  style="max-width:100px">
+                <option >------</option>
+                <option value="9:00">9:00</option>
+                <option value="9:15">9:15</option>
+                <option value="9:30">9:30</option>
+                <option value="9:45">9:45</option>
+                <option value="10:00">10:00</option>
+                <option value="10:15">10:15</option>
+                <option value="10:30">10:30</option>
+                <option value="10:45">10:45</option>
+                <option value="11:00">11:00</option>
+                <option value="11:15">11:15</option>
+                <option value="11:30">11:30</option>
+                <option value="11:45">11:45</option>
+                <option value="13:30">13:30</option>
+                <option value="13:45">13:45</option>
+                <option value="14:00">14:00</option>
+                <option value="14:15">14:15</option>
+                <option value="14:30">14:30</option>
+                <option value="14:45">14:45</option>
+                <option value="15:00">15:00</option>
+                <option value="15:15">15:15</option>
+                <option value="15:30">15:30</option>
+                <option value="15:45">15:45</option>
+                <option value="16:00">16:00</option>
+                <option value="16:15">16:15</option>
+                <option value="16:30">16:30</option>
+                <option value="16:45">16:45</option>
             </select> <br> <br> </div>
 
 
@@ -71,30 +71,30 @@
             <label>Klinik Seçiniz</label> <br> <br>
             <select name="department" style="max-width:350px">
                 <option value="0">------</option>
-                <option value="1">Cerrahi Onkolojisi</option>
-                <option value="2">Çocuk İmmünolojisi ve Alerji Hastalıkları</option>
-                <option value="3">Çocuk Sağlığı ve Hastalıkları</option>
-                <option value="4">Deri ve Zührevi Hastalıkları (Cildiye)</option>
-                <option value="5">>Diş Hekimliği (Genel Diş)</option>
-                <option value="6">Enfeksiyon Hastalıkları ve Klinik Mikrobiyoloji</option>
-                <option value="7">Fiziksel Tıp ve Rehabilitasyon</option>
-                <option value="8">Gastroenteroloji</option>
-                <option value="9">Genel Cerrahi</option>
-                <option value="10">Göğüs Cerrahisi</option>
-                <option value="11">Göğüs Hastalıkları</option>
-                <option value="12">Göz Hastalıkları</option>
-                <option value="13">İç Hastalıkları (Dahiliye)</option>
-                <option value="14">İmmünoloji ve Alerji Hastalıkları</option>
-                <option value="15">Kadın Hastalıkları ve Doğum</option>
-                <option value="16">Kalp ve Damar Cerrahisi</option>
-                <option value="17">Kardiyoloji</option>
-                <option value="18">Kulak Burun Boğaz Hastalıkları</option>
-                <option value="19">Nöroloji</option>
-                <option value="20">Ortopedi ve Travmatoloji</option>
-                <option value="21">Plastik, Rekonstrüktif ve Estetik Cerrahi</option>
-                <option value="22">Romatoloji</option>
-                <option value="23">>Ruh Sağlığı ve Hastalıkları (Psikiyatri)</option>
-                <option value="24">Sigarayı Bıraktırma Kliniği</option>
+                <option value="Cerrahi Onkolojisi">Cerrahi Onkolojisi</option>
+                <option value="Çocuk İmmünolojisi ve Alerji Hastalıkları">Çocuk İmmünolojisi ve Alerji Hastalıkları</option>
+                <option value="Çocuk Sağlığı ve Hastalıkları">Çocuk Sağlığı ve Hastalıkları</option>
+                <option value="Deri ve Zührevi Hastalıkları (Cildiye)">Deri ve Zührevi Hastalıkları (Cildiye)</option>
+                <option value="Diş Hekimliği (Genel Diş)">>Diş Hekimliği (Genel Diş)</option>
+                <option value="Enfeksiyon Hastalıkları ve Klinik Mikrobiyoloji">Enfeksiyon Hastalıkları ve Klinik Mikrobiyoloji</option>
+                <option value="Fiziksel Tıp ve Rehabilitasyon">Fiziksel Tıp ve Rehabilitasyon</option>
+                <option value="Gastroenteroloji">Gastroenteroloji</option>
+                <option value="Genel Cerrahi">Genel Cerrahi</option>
+                <option value="Göğüs Cerrahisi">Göğüs Cerrahisi</option>
+                <option value="Göğüs Hastalıkları">Göğüs Hastalıkları</option>
+                <option value="Göz Hastalıkları">Göz Hastalıkları</option>
+                <option value="İç Hastalıkları (Dahiliye)">İç Hastalıkları (Dahiliye)</option>
+                <option value="İmmünoloji ve Alerji Hastalıkları">İmmünoloji ve Alerji Hastalıkları</option>
+                <option value="Kadın Hastalıkları ve Doğum">Kadın Hastalıkları ve Doğum</option>
+                <option value="Kalp ve Damar Cerrahisi">Kalp ve Damar Cerrahisi</option>
+                <option value="Kardiyoloji">Kardiyoloji</option>
+                <option value="Kulak Burun Boğaz Hastalıkları">Kulak Burun Boğaz Hastalıkları</option>
+                <option value="Nöroloji">Nöroloji</option>
+                <option value="Ortopedi ve Travmatoloji">Ortopedi ve Travmatoloji</option>
+                <option value="Plastik, Rekonstrüktif ve Estetik Cerrahi">Plastik, Rekonstrüktif ve Estetik Cerrahi</option>
+                <option value="Romatoloji">Romatoloji</option>
+                <option value="Ruh Sağlığı ve Hastalıkları (Psikiyatri)">Ruh Sağlığı ve Hastalıkları (Psikiyatri)</option>
+                <option value="Sigarayı Bıraktırma Kliniği">Sigarayı Bıraktırma Kliniği</option>
             </select> <br> <br>
         </div>
 
