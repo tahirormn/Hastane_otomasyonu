50688b983070f5b00490ac19529402905e3297fd
Ahmetgrkm <ahmetgrkm1076@gmail.com>
2023-06-05 00:00:15 +0300

    tasarım
    hekimlerimiz ve yönetim kurulu tasarımı bitti

--- hospitalAutomation/resources/views/boardOfDirector.blade.php
+++ hospitalAutomation/resources/views/boardOfDirector.blade.php
@@ -1,17 +1,31 @@
-@extends('layouts.app')
-@section('title')
-    Board Of Director
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{url('css/boardOFDirector/boardOFDirector.css')}}">
+    <title>Board of Directors</title>
+</head>
+<body>
+<div class="box">
+    <div class="hekim">
+        <h2>Yönetim Kurulu</h2>
+        <p>Başhekim Opr. Dr. Sabri Çalışkan</p>
+        <p>Başhekim Yardımcısı Doç.  Dr. Hanım Çalışkan</p>
+        <p>Hastane Müdürü Doç. Dr. Tahir Orman </p>
+        <p>Başhemşire Opr. Dr.Tuğba Ayvalı  </p>
+        <p>Başhemşire Yardımcısı Doç. Dr. Abdurrahman Toprak  </p>
 
-@section('css')
-    {{url('css/home/style.css')}}
-@endsection
-@section('css2')
-    {{url('css/register/register.css')}}
-@endsection
 
-@section('body')
+    </div>
+</div>
+
+</body>
+</html>
 
 
-@endsection
+</body>
+</html>
+
 
